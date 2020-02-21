6cf8da66ef3cec1282c958c4b0692edd09538abc
crane719 <tmp.utils@gmail.com>
2020-02-21 21:43:14 +0900

    bug fixed

--- main/forms.py
+++ main/forms.py
@@ -12,12 +12,25 @@ class MultipleUploadForm(forms.Form):
 
     def save(self):
         file_paths = []
-        for upload_file in self.files.getlist("file"):
+        names = []
+        for i, upload_file in enumerate(self.files.getlist("file")):
             name = upload_file.name.replace(" ", "")
+            name = name.replace(".", "")
+            origin_name = name.replace("mp4", "")
+
+            name = "%d.mp4"%(i)
             file_name = default_storage.save(name, upload_file)
             file_path = default_storage.url(file_name)
             file_path = "." + file_path
+            #file_path = file_path.split("/")[1:]
+            #tmp = ""
+            #for i, path in enumerate(file_path):
+            #    tmp += path
+            #    if i != len(file_path)-1:
+            #        tmp += "/"
+            #file_path = tmp
             file_paths.append(file_path)
+            names.append(origin_name)
         export_dir = self.cleaned_data["export_dir"]
-        hoge.video2mp3(file_paths, export_dir)
+        hoge.video2mp3(names, file_paths, export_dir)
         hoge.recreate_dir(["./media"])
